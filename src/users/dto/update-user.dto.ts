4fdc5455cbda51fbe4c1252bdf84a7e2e6574da5
dossantoscarlos <anjoazrael10@gmail.com>
2022-11-20 08:11:25 -0300

    refatorando codigo com identacao padronizada

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,11 +3,11 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    @IsEmail()
-    @IsNotEmpty()
-    email: string;
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
 
-    @IsNotEmpty()
-    @IsString()
-    password: string;
+  @IsNotEmpty()
+  @IsString()
+  password: string;
 }
